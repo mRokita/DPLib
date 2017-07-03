3fa1f3b64ebb5d4fcf291ef6e70d6b49ddcf74ae
mrokita <mrokita@users.noreply.github.com>
2017-07-04 01:08:38 +0200

    v1.3

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@
 from distutils.core import setup
 
 setup(name='DPLib',
-      version='1.21',
+      version='1.3',
       description='Asynchronous bot framework for Digital Paint: Paintball 2 serversPython Distribution Utilities',
       author='Michał Rokita',
       author_email='mrokita@mrokita.pl',
       url='https://mrokita.github.io/DPLib/',
       packages=['dplib'],
       keywords=['digital', 'paint', 'paintball 2', 'jitspoe', 'dp2', 'DP:PB2'],
-      download_url = 'https://github.com/mRokita/DPLib/tarball/1.1',
+      download_url = 'https://github.com/mRokita/DPLib/tarball/1.3',
       classifiers=[
             'Programming Language :: Python :: 3',
             'Intended Audience :: Developers',
